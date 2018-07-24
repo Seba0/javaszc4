97ed2278eca93db5340b6c018adb4fbea518ee97
Seba_0 <31700923+Seba0@users.noreply.github.com>
2018-07-24 22:11:01 +0200

    Komitowanie zadania nr 8 i 9

---
+++ src/javaszc4/zadania/podstawy/_5/Ksiazka.java
@@ -0,0 +1,51 @@
+package javaszc4.zadania.podstawy._5;
+
+public class Ksiazka {
+
+    private String tytul;
+    private String autor;
+    private int liczbaStron;
+    private String wydawnictwo;
+
+    public String getTytul() {
+        return tytul;
+    }
+
+    public void setTytul(String tytul) {
+        this.tytul = tytul;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getLiczbaStron() {
+        return liczbaStron;
+    }
+
+    public void setLiczbaStron(int liczbaStron) {
+        this.liczbaStron = liczbaStron;
+    }
+
+    public String getWydawnictwo() {
+        return wydawnictwo;
+    }
+
+    public void setWydawnictwo(String wydawnictwo) {
+        this.wydawnictwo = wydawnictwo;
+    }
+
+    @Override
+    public String toString() {
+        return "Ksiazka{" +
+                "tytul='" + tytul + '\'' +
+                ", autor='" + autor + '\'' +
+                ", liczbaStron=" + liczbaStron +
+                ", wydawnictwo='" + wydawnictwo + '\'' +
+                '}';
+    }
+}
