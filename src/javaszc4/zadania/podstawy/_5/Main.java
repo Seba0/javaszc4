830e74db76c3dc3d1604af70b64d71518bb0f4a9
Seba_0 <31700923+Seba0@users.noreply.github.com>
2018-07-24 19:57:27 +0200

    Komitowanie zadania nr 3

--- src/javaszc4/zadania/podstawy/_5/Main.java
+++ src/javaszc4/zadania/podstawy/_5/Main.java
@@ -1,12 +1,15 @@
 package javaszc4.zadania.podstawy._5;
 
+import javaszc4.zadania.podstawy._5.rasy.RasaKota;
+import javaszc4.zadania.podstawy._5.rasy.RasaPsa;
+
 public class Main {
 
     public static void main(String[] args) {
         // Klasy i dziedziczenie
         // Zadanie 1
-        Pies pies = new Pies("Pesek", 2);
-        Kot kot = new Kot("Kotek", 3);
+        Pies pies = new Pies("Pesek", 2, RasaPsa.AIREDALE_TERRIER);
+        Kot kot = new Kot("Kotek", 3, RasaKota.KOT_MANX);
 
         System.out.println(pies);
         System.out.println(kot);
