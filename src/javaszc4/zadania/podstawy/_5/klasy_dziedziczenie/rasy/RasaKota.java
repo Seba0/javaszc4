a18ba7b0995808f9e8f8758cb796c015463076fe
Seba_0 <31700923+Seba0@users.noreply.github.com>
2018-08-01 10:19:27 +0200

    Zadania zostały przeniesione do osobnej gałęzi

--- src/javaszc4/zadania/podstawy/_5/klasy_dziedziczenie/rasy/RasaKota.java
+++
@@ -1,8 +0,0 @@
-package javaszc4.zadania.podstawy._5.klasy_dziedziczenie.rasy;
-
-/**
- * @see <a href="https://www.koty.pl/rasy/">Rasy kotów</a>
- */
-public enum RasaKota {
-    KOT_MANX, KOT_PIXIE_BOB, TOYGER
-}
--- src/javaszc4/zadania/podstawy/_5/klasy_dziedziczenie/rasy/RasaPsa.java
+++
@@ -1,9 +0,0 @@
-package javaszc4.zadania.podstawy._5.klasy_dziedziczenie.rasy;
-
-/**
- * @see <a href="https://www.psy.pl/rasy/">Rasy psów</a>
-  */
-
-public enum RasaPsa {
-    POLSKI_OWCZAREK_NIZINNY, AMERYKANSKI_PITBULTERIER, AIREDALE_TERRIER
-}
