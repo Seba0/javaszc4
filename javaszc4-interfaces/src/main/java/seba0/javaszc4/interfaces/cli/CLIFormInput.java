7740655697b3a1d0de24a4de747b2fe1a04ed150
Seba_0 <31700923+Seba0@users.noreply.github.com>
2018-08-11 10:07:27 +0200

    Nowa gałąź dla zadań na zajęciach

--- javaszc4-interfaces/src/main/java/seba0/javaszc4/interfaces/cli/CLIFormInput.java
+++
@@ -1,7 +0,0 @@
-package seba0.javaszc4.interfaces.cli;
-
-public interface CLIFormInput {
-    String getLabel();
-
-    boolean isValid(String value);
-}
--- javaszc4-malafirma/malafirma-przychodnia/src/main/java/seba0/javaszc4/malafirma/przychodnia/pracownicy/Lekarz.java
+++
@@ -1,7 +0,0 @@
-package seba0.javaszc4.malafirma.przychodnia.pracownicy;
-
-import seba0.javaszc4.malafirma.kadry.pracownicy.Pracownik;
-
-public class Lekarz extends Pracownik {
-
-}
