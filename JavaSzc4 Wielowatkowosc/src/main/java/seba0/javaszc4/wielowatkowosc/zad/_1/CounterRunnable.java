df3257a399a40221c92e429bd0cdf809c4ba6f19
Seba_0 <31700923+Seba0@users.noreply.github.com>
2018-09-30 11:24:58 +0200

    Wielowątkowość Przerwania

---
+++ JavaSzc4 Wielowatkowosc/src/main/java/seba0/javaszc4/wielowatkowosc/zad/_1/CounterRunnable.java
@@ -0,0 +1,26 @@
+package seba0.javaszc4.wielowatkowosc.zad._1;
+
+public class CounterRunnable implements Runnable {
+    private final int countTo;
+    private final int delayMillis;
+
+    public CounterRunnable(int countTo, int delayMillis) {
+        this.countTo = countTo;
+        this.delayMillis = delayMillis;
+    }
+
+    @Override
+    public void run() {
+        String name = getClass().getSimpleName();
+        try {
+            for (int i = 0; i < countTo; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(name + ": " + i);
+                    Thread.sleep(delayMillis);
+                }
+            }
+        } catch (InterruptedException e) {
+            System.out.println(name + ": " + e.getMessage());
+        }
+    }
+}
---
+++ JavaSzc4 Wielowatkowosc/src/main/java/seba0/javaszc4/wielowatkowosc/zad/_1/Main.java
@@ -0,0 +1,25 @@
+package seba0.javaszc4.wielowatkowosc.zad._1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Inser number to count to number: ");
+        while (!scanner.hasNextInt()) {
+            scanner.nextLine();
+            System.out.print("\nInser count number: ");
+        }
+        int countTo = scanner.nextInt();
+
+        Thread runThread1 = new Thread(new CounterRunnable(countTo, 1500));
+        Thread thrThread2 = new CounterThread(countTo, 2000);
+
+        runThread1.start();
+        thrThread2.start();
+
+        Thread.sleep(10000);
+
+        thrThread2.interrupt();
+    }
+}
