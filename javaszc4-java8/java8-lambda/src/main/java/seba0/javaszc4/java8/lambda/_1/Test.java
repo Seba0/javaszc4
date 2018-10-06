d2db34ce6d7eb9defd26fce9755ec38cdea70009
Seba_0 <31700923+Seba0@users.noreply.github.com>
2018-10-06 15:33:49 +0200

    Java 8 - Lambdy

---
+++ javaszc4-java8/java8-lambda/src/main/java/seba0/javaszc4/java8/lambda/_1/Test.java
@@ -0,0 +1,5 @@
+package seba0.javaszc4.java8.lambda._1;
+
+public interface Test {
+    Test test();
+}
---
+++ javaszc4-java8/java8-lambda/src/main/java/seba0/javaszc4/java8/lambda/_2/Provider.java
@@ -0,0 +1,5 @@
+package seba0.javaszc4.java8.lambda._2;
+
+public interface Provider {
+    String provide(String lastName, String firstName);
+}
